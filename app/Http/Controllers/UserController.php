84884d97ef7e2a0bbc86a23b6543ab7d01571dcf
Addin15 <addin.utb15@gmail.com>
2022-05-07 21:20:34 +0800

    update landing page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,25 +9,27 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    function login(Request $request) {
-        $user= User::where('email', $request->email)->first();
-            if (!$user || !Hash::check($request->password, $user->password)) {
-                return response([
-                    'message' => ['These credentials do not match our records.']
-                ], 404);
-            }
-        
-             $token = $user->createToken('my-app-token')->plainTextToken;
-        
-            $response = [
-                'user' => $user,
-                'token' => $token
-            ];
-        
-             return response($response, 201);
+    function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
     }
 
-    function register(Request $request) {
+    function register(Request $request)
+    {
         $fields = $request->validate([
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string'
@@ -48,7 +50,8 @@ class UserController extends Controller
         return response($response, 201);
     }
 
-    function logout(Request $request) {
+    function logout(Request $request)
+    {
         auth('sanctum')->user()->tokens()->delete();
 
         return [
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,14 +2,13 @@
 <html>
 
     <head>
-        <title>myKhairat</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>myKhairat</title>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -20,38 +19,94 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <link rel="stylesheet" href="css\styles.css">
+        <link rel="stylesheet" href="css\Footer-Basic.css">
+
+        <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+        </style>
     </head>
 
     <body>
-        <div class="container sticky-top ">
-            <div class="flex row">
-                <div class="col-lg pt-3 ps-3">myKhairat</div>
-                <div class="d-flex justify-content-end pt-3 pe-3 col">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        </li>
+        <nav class="navbar navbar-light navbar-expand-md">
+            <div class="container"><a class="navbar-brand" href="#" style="font-weight: bold;">myKhairat</a><button
+                    data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span
+                        class="visually-hidden">Toggle navigation</span><span
+                        class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse d-md-flex justify-content-md-end" id="navcol-1">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="#">About</a></li>
+                        <li class="nav-item"></li>
                     </ul>
                 </div>
             </div>
-        </div>
-        <div class="container mt-3">
-            <div class="row justify-content-center bg-primary">
-                <!-- Download section -->
-                <div class="col bg-secondary pt-5">
-                    Download
+        </nav>
+        <div class="container-fluid" style="margin-top: 20px;margin-bottom: 50px;">
+            <div class="row">
+                <div class="col-md-6 d-flex flex-column flex-fill align-items-xxl-center"
+                    style="padding-top: 150px;padding-bottom: 150px;padding-right: 30px;padding-left: 30px;border-style: none;">
+                    <p>Muat turun di</p>
+                    <div class="text-center float-none d-xxl-flex justify-content-xxl-center align-items-xxl-center download-button"
+                        data-bss-hover-animate="pulse"
+                        style="height: 80px;border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;border: 2px solid rgb(78,214,155);margin-right: 100px;margin-left: 100px;width: 300px;">
+                        <img class="d-none float-start d-xxl-flex align-items-center align-self-center justify-content-xxl-center align-items-xxl-center"
+                            style="width: 64px;height: 64px;margin-left: 15px;" src="img/google-play.png">
+                        <h3
+                            class="text-center d-inline-flex float-end d-xxl-flex flex-grow-1 justify-content-xxl-center">
+                            Google Play</h3>
+                    </div>
+                    <div class="text-center float-none d-xxl-flex justify-content-xxl-center align-items-xxl-center download-button"
+                        data-bss-hover-animate="pulse"
+                        style="height: 80px;border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-right-radius: 15px;border-bottom-left-radius: 15px;border: 2px solid rgb(78,214,155);margin-right: 100px;margin-left: 100px;width: 300px;margin-top: 15px;">
+                        <img class="d-none float-start d-xxl-flex align-items-center align-self-center justify-content-xxl-center align-items-xxl-center"
+                            style="width: 64px;height: 64px;margin-left: 15px;" src="img/app-store.png">
+                        <h3 class="text-center d-inline-flex float-end d-xxl-flex flex-grow-1 justify-content-xxl-center"
+                            style="color: var(--bs-secondary);">Coming Soon</h3>
+                    </div>
                 </div>
-                <!-- Welcome section -->
-                <div class="col">
-                    SELAMAT DATANG KE<br>
-                    MYKHAIRAT
+                <div class="col-md-6" style="padding-top: 80px;padding-right: 150px;">
+                    <h1>SELAMAT DATANG KE</h1>
+                    <h1 style="color: RGB(78,214,155);">MYKHAIRAT</h1>
+                    <p style="margin-top: 15px;">Lorem ipsum dolor sit amet. 33 magni temporibus nam perspiciatis
+                        aperiam non sunt officia eos Quis obcaecati. Qui aperiam aperiam quo architecto adipisci aut
+                        odit sunt a repudiandae doloribus aut ipsa rerum aut distinctio accusantium quo quas
+                        exercitationem. Quo nulla quia 33 enim quaerat ut velit voluptatum eos placeat fuga eos laborum
+                        distinctio! Et illo quasi aut tempora consectetur aut accusamus sapiente hic placeat sunt.Est
+                        mollitia corporis qui maiores perspiciatis ut saepe voluptatem. Eum corrupti voluptatem aut
+                        nihil provident est ullam corporis non cumque voluptatem!Ab voluptas dolor aut provident quas
+                        aut rerum necessitatibus!&nbsp;<br><br></p>
+                    <div class="d-xxl-flex justify-content-xxl-center">
+                        <div class="col d-xxl-flex flex-column justify-content-xxl-center align-items-xxl-center">
+                            <button class="btn btn-primary bg-success bg-gradient d-xxl-flex" type="button"
+                                style="background: rgb(78,214,155);border-radius: 11px;padding: 10px;padding-right: 25px;padding-left: 25px;max-width: 250px;text-align: center;">Log
+                                Masuk sebagai Admin</button>
+                            <div class="d-flex" style="padding-top: 5px;">
+                                <p>Atau&nbsp;</p><a href="#"
+                                    style="color: rgb(78,214,155);font-weight: bold; text-decoration: none;">Daftar</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <footer class="footer-basic">
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i
+                        class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a>
+            </div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">myKhairat © 2022</p>
+        </footer>
+        <script src="js/bs-init.js"></script>
     </body>
 
 </html>
