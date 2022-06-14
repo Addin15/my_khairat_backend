b0bbea2e828eb2b7680baab2aebe469e26a9ad60
bwikimpeloles <exopink0394@gmail.com>
2022-06-14 17:49:21 +0800

    Update Dependent.php

--- app/Models/Dependent.php
+++ app/Models/Dependent.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Dependent extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'dependent_name',
+        'dependent_relation',
+        'dependent_ic',
+        'dependent_phone',
+        'dependent_occupation',
+        'dependent_address',
+        'death_status',
+        'death_date',
+    ];
 }
