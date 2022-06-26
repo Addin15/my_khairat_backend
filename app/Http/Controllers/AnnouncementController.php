c76c83d14b69c464da79ea705c0dcc7b9a117756
Addin15 <addin.utb15@gmail.com>
2022-06-26 08:02:50 +0800

    add image upload for announcement

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Announcement;
+use Illuminate\Support\Facades\Storage;
 
 class AnnouncementController extends Controller
 {
@@ -20,13 +21,39 @@ class AnnouncementController extends Controller
     function addAnnouncement(Request $request) {
         $mosque = request('mosque_id');
 
-        $response = Announcement::create([
-            'mosque_id' => $mosque,
-            'announcement_title' => request('announcement_title'),
-            'announcement_content' => request('announcement_content'),
-            'announcement_img_url' => request('announcement_img_url'),
-            'announcement_date' => request('announcement_date'),
-        ]);
+        if($request->hasFile('image')) {
+          
+            //get filename with extension
+            $filenamewithextension = $request->file('image')->getClientOriginalName();
+      
+            //get filename without extension
+            $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
+      
+            //get file extension
+            $extension = $request->file('image')->getClientOriginalExtension();
+      
+            //filename to store
+            $filenametostore = $filename.'_'.uniqid().'.'.$extension;
+      
+            //Upload File to external server
+            Storage::disk('ftp')->put('announcement_images/'.$mosque.'/'.$filenametostore, fopen($request->file('image'), 'r+'));
+      
+            $response = Announcement::create([
+                'mosque_id' => $mosque,
+                'announcement_title' => request('announcement_title'),
+                'announcement_content' => request('announcement_content'),
+                'announcement_date' => request('announcement_date'),
+                'announcement_img_url' => '/images/announcement_images/'.$mosque.'/'.$filenametostore,
+            ]);
+
+        } else {
+            $response = Announcement::create([
+                'mosque_id' => $mosque,
+                'announcement_title' => request('announcement_title'),
+                'announcement_content' => request('announcement_content'),
+                'announcement_date' => request('announcement_date'),
+            ]);
+        }
 
         return response($response, 201);
     }
